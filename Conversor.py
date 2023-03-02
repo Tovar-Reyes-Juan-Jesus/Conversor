39026fe8846e5ee8f147ec3a6eca8f9f0f1d18d6
Tovar-Reyes-Juan-de-Jesus <za210110162@zapopan.tecmm.edu.mx>
2023-03-02 12:52:58 -0600

    first commit

---
+++ Conversor.py
@@ -0,0 +1,20 @@
+#App para convertir Pies a Metros usando Tkinter
+#Tovar Reyes Juan de Jesus
+#23/Feb/2023.
+
+from tkinter import *     #estos son los paquetes a importar.
+from tkinter import ttk
+
+class Conversor:
+    #__init__ es el contructor pero para la version de python. siempre el primer parametro siempre sera el "self"
+    #tipo de constructor de la clase
+    def __init__(self, raiz):
+        raiz.title("Conversor de Pies a Metros")
+
+if __name__=="__main__": #este sirve para declararlo como main por asi decirlo.
+
+     print("Este es el archivo principal.")
+     print("Nada mas se mostrara esto si es el principal.")
+
+
+
---
+++ Principal.py
@@ -0,0 +1,8 @@
+from tkinter import *
+from tkinter import Tk
+from Conversor import Conversor
+#    ARCHIVO          CLASE
+
+raiz = Tk()
+Conversor(raiz) #aqui es mandarle el parametro donde podra recibir en la clase conversor.
+raiz.mainloop()
